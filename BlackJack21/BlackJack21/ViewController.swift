b76a8bf59e6165e4c6b1a9e137df02004b855a24
Kushal Jogi <jogikushal@gmail.com>
2015-04-02 05:20:21 -0400

    Auto Layout and MultiPlayer
    Works in both Landscape as well as portrait mode.

--- BlackJack21/BlackJack21/ViewController.swift
+++ BlackJack21/BlackJack21/ViewController.swift
@@ -11,23 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    
+    
     @IBOutlet weak var dealerHandView: UIView!
     
     
     @IBOutlet weak var dealerCard: UIImageView!
     
+    
+    
     @IBOutlet weak var playerView: UIView!
+    
     
     @IBOutlet weak var pCard: UIImageView!
     
     @IBOutlet weak var aCard: UIImageView!
     
-   
     @IBOutlet weak var oneButton: UIButton!
     
-    
     @IBOutlet weak var fiveButton: UIButton!
-    
+   
     @IBOutlet weak var dealButton: UIButton!
     
     @IBOutlet weak var standButton: UIButton!
@@ -39,6 +42,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var betAmount: UILabel!
     
     @IBOutlet weak var nextButton: UIButton!
+    
     
     var players : [Player] = [Player]()
     var dealer : Dealer = Dealer(dname: "dealer", dhands: Hand())
@@ -61,6 +65,7 @@ class ViewController: UIViewController {
     
     
     
+    
     @IBAction func nextPressed(sender: AnyObject) {
         
             /*   let newCardView3 : UIView = helper.createCardSubView(x + (CGFloat(0)*xoffSet) , y:y,width:width1,height:height1,imageName : "back")
@@ -71,19 +76,26 @@ class ViewController: UIViewController {
     }
     
     func clear(){
-        players[playingPlayer].hands.bet = 0
-        players[playingPlayer].hands.cards = []
+        for j in 0...1 {
+        players[j].hands.bet = 0
+        players[j].hands.cards = []
         dealer.hands.cards = []
-        
+        }
         for subUIView in playerView.subviews as [UIView] {
+            if (subUIView.tag == 100){
             subUIView.removeFromSuperview()
+            }
+          
         }
         for subUIView in dealerHandView.subviews as [UIView] {
-            subUIView.removeFromSuperview()
+            if (subUIView.tag == 100){
+                subUIView.removeFromSuperview()
+            }
+
         }
     }
     
-
+ 
     @IBAction func oneButtonPressed(sender: AnyObject) {
         print("one pressed")
         players[playingPlayer].money -= 1
@@ -94,6 +106,8 @@ class ViewController: UIViewController {
             dealButton.enabled = true
         }
     }
+    
+   
     @IBAction func fiveButtonPressed(sender: AnyObject) {
         print("five pressed")
         players[playingPlayer].money -= 5
@@ -105,6 +119,7 @@ class ViewController: UIViewController {
         }
     }
     
+    
     @IBAction func dealButtonPressed(sender: AnyObject) {
         print("deal pressed")
         hitButton.enabled = true
@@ -114,7 +129,12 @@ class ViewController: UIViewController {
             players[playingPlayer].hands.cards.append(a)
             showCards(playerView, currentView: pCard, isPlayer: true)
         }
-        showCards(playerView, currentView: pCard, isPlayer: true)
+        for j in 1...2{
+            var a = shoe.removeLast()
+            players[1].hands.cards.append(a)
+            showCards(playerView, currentView: aCard, isPlayer: true)
+        }
+        
         
         var b = shoe.removeLast()
         dealer.hands.cards.append(b)
@@ -123,12 +143,14 @@ class ViewController: UIViewController {
         dealButton.enabled = false
     }
     
+    
     @IBAction func standButtonPressed(sender: AnyObject) {
         print("stand pressed")
         nextButton.enabled = true
         standButton.enabled = false
         hitButton.enabled = false
         dealerPick()
+        aiPick()
         win()
         
         players[playingPlayer].hands.bet = 0
@@ -138,6 +160,7 @@ class ViewController: UIViewController {
     }
    
     
+    
     @IBAction func hitButtonPressed(sender: AnyObject) {
         print("hit pressed")
         var a = shoe.removeLast()
@@ -145,11 +168,13 @@ class ViewController: UIViewController {
         showCards(playerView, currentView: pCard, isPlayer: true)
         
         if players[playingPlayer].hands.score > 21 {
+            aiPick()
             var d = shoe.removeLast()
             
             dealer.hands.cards.append(d)
             showCards(dealerHandView, currentView: dealerCard, isPlayer: false)
             win()
+            
         }
     }
     
@@ -161,10 +186,22 @@ class ViewController: UIViewController {
             showCards(dealerHandView, currentView: dealerCard, isPlayer: false)
             if dealer.hands.score > 21{
                 //dealerBusted()
-                
+                win()
             }
             dealerPick()
         }
+    }
+    
+    func aiPick(){
+        if players[1].hands.score < 16{
+            var d = shoe.removeLast()
+            
+            players[1].hands.cards.append(d)
+            showCards(playerView, currentView: aCard, isPlayer: true)
+            
+            aiPick()
+        }
+        
     }
     
     
@@ -183,13 +220,16 @@ class ViewController: UIViewController {
         let helper = ViewControllerHelper()
         //if(addAllCards){
         if(isPlayer){
-            
+            if (currentView == aCard){
+                playingPlayer = 1
+            }
             for i in players[playingPlayer].hands.cards{
         
                 let newCardView : UIView = helper.createCardSubView(x + (CGFloat(j++)*xoffSet) , y:y,width:width1,height:height1,imageName : "\(i.rank.values.r)\(i.suite.rawValue)")
                 parentView.addSubview(newCardView)
+                    newCardView.tag = 100
             }
-            
+            playingPlayer = 0
         }
             
         else{
@@ -197,14 +237,16 @@ class ViewController: UIViewController {
                 for k in dealer.hands.cards{
                     let newCardView : UIView = helper.createCardSubView(x + (CGFloat(j++)*xoffSet) , y:y,width:width1,height:height1,imageName : "\(k.rank.values.r)\(k.suite.rawValue)")
                     parentView.addSubview(newCardView)
-                    
+                    newCardView.tag = 100
                 }
                 let newCardView1 : UIView = helper.createCardSubView(x + (CGFloat(j++)*xoffSet) , y:y,width:width1,height:height1,imageName : "back")
                 parentView.addSubview(newCardView1)
+                newCardView1.tag = 100
             } else{
                 for k in dealer.hands.cards{
                     let newCardView2 : UIView = helper.createCardSubView(x + (CGFloat(j++)*xoffSet) , y:y,width:width1,height:height1,imageName : "\(k.rank.values.r)\(k.suite.rawValue)")
                     parentView.addSubview(newCardView2)
+                    newCardView2.tag = 100
                 }
             }
             
@@ -215,8 +257,11 @@ class ViewController: UIViewController {
     
     
     func win() {
+        standButton.enabled = false
+        hitButton.enabled = false
+        nextButton.enabled = true
         if players[playingPlayer].hands.score > dealer.hands.score && (players[playingPlayer].hands.score < 21 || players[playingPlayer].hands.score == 21){
-            var alert = UIAlertController(title: "Win", message: "Player \(players[playingPlayer].name) Wins", preferredStyle: UIAlertControllerStyle.Alert)
+            var alert = UIAlertController(title: "Win", message: "Player Wins", preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "Next", style: UIAlertActionStyle.Default, handler: nil))
             self.presentViewController(alert, animated: true, completion: nil)
         } else if dealer.hands.score > players[playingPlayer].hands.score && (dealer.hands.score < 21 || dealer.hands.score == 21){
@@ -228,13 +273,13 @@ class ViewController: UIViewController {
             alert.addAction(UIAlertAction(title: "Next", style: UIAlertActionStyle.Default, handler: nil))
             self.presentViewController(alert, animated: true, completion: nil)
         } else if dealer.hands.score > 21 {
-            var alert = UIAlertController(title: "Dealer Busted", message: "Player \(players[playingPlayer].name) Wins", preferredStyle: UIAlertControllerStyle.Alert)
+            var alert = UIAlertController(title: "Dealer Busted", message: "Player Wins", preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "Next", style: UIAlertActionStyle.Default, handler: nil))
             self.presentViewController(alert, animated: true, completion: nil)
             //  let pcards = players[playingPlayer].hands.cards
             players[playingPlayer].money += players[playingPlayer].hands.bet
         } else if players[playingPlayer].hands.score > 21{
-            var alert = UIAlertController(title: "Busted", message: "Player \(players[playingPlayer].name) Busted", preferredStyle: UIAlertControllerStyle.Alert)
+            var alert = UIAlertController(title: "Busted", message: "Player Busted", preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "Next", style: UIAlertActionStyle.Default, handler: nil))
             self.presentViewController(alert, animated: true, completion: nil)
             //  let pcards = players[playingPlayer].hands.cards
